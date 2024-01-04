cf6da2f0503c39ff26606ba8edfbf1869df65388
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-01-04 12:11:16 -0800

    workshop solutions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + '/index.html')
 });
 
 // hello world route
@@ -21,31 +21,31 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets)
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const petOwner = req.query.owner
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const pet = pets.find(pet => pet.owner === petOwner);
 
     // send the pet as a response
-
+    res.send(`${petOwner} is the owner of ${pet.name}.`)
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const petName = req.params.name
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
+    const pet = pets.find(pet => pet.name === petName);
 
     // send the pet as a response
-
+    res.send(`${petName} is the name of ${pet.name}`)
 });
 
 app.listen(PORT, () => {
@@ -53,3 +53,18 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+/* Pseudocode for each prompt: 
+
+1. GET - all pets - '/api/v1/pets' 
+    Send the array of pets as the response 
+
+2. GET - pet by id - '/api/v1/pets/:name'
+    Send a request to get the owner from the pet array
+    And then send the owner as the response
+
+3. GET - pet by owner name - '/api/v1/pets/owner'
+    Send a request to get the pet name from the pet array
+    And then send the pet as the response
+
+*/
